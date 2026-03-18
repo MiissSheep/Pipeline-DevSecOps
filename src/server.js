8b380c97f515bf0ff305f820593118c25109c86f
MiissSheep <100830435+MiissSheep@users.noreply.github.com>
2026-03-19 00:15:56 +0100

    TEST3 : SQL injection vulnerability

--- src/server.js
+++ src/server.js
@@ -67,5 +67,18 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
+// Inj SQL
+const mysql = require("mysql");
+const db = mysql.createConnection({});
+
+app.get("/test-sqli", (req, res) => {
+  const id = req.query.id;
+
+  // ⚠️ volontairement vulnérable
+  db.query("SELECT * FROM users WHERE id = " + id, (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.json(result);
+  });
+});
 
 app.listen(3000, () => console.log('✅ Secure server running'));
